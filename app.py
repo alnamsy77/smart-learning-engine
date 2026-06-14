4fb273e22ec006280f5ab48a48bc0dc0231594d1
alnamsy77 <wateengalpy@gmail.com>
2026-06-14 21:07:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -358,7 +358,7 @@ def dashboard():
             <td class="{result_class}">{ar_result(r.get('result'))}</td>
         </tr>
         """
-      return f"""
+          return f"""
         <!doctype html>
         <html lang="ar" dir="rtl">
     <head>
